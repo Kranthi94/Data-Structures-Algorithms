52e6d4d9edcc02d271d3cb155e8c8ed96510542e
Kranthi <studymaterials0894@gmail.com>
2025-02-17 05:49:51 +0530

    Data Structures - Stacks

---
+++ src/datastructures/stack/StackInterface.kt
@@ -0,0 +1,11 @@
+package datastructures.stack
+
+interface StackInterface<T> {
+    fun getSize(): Int
+    fun isEmpty(): Boolean
+    fun isFull(): Boolean
+    fun push(value: T): Boolean
+    fun peek(): T
+    fun pop(): T
+    fun print()
+}
